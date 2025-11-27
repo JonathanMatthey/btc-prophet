f6f9159c3b5518478897e07c363e76783c78d848
Jonathan Matthey <mattheyj@gmail.com>
2025-11-27 14:47:00 -0500

    component exports

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+export { Header } from './Header';
+export { Footer } from './Footer';
+export { PriceCard } from './PriceCard';
+export { ScoreCard } from './ScoreCard';
+export { GameControls } from './GameControls';
+
